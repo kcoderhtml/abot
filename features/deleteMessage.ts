4f7267a4ed2288482aa9248e2e82b1122fff18c7
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-06-28 17:01:29 -0400

    feat: Add deleteMessage feature to handle deleting messages in a thread

---
+++ features/deleteMessage.ts
@@ -0,0 +1,45 @@
+import { SlackApp } from "slack-edge";
+
+const deleteMessage = async (
+    app: SlackApp<{
+        SLACK_SIGNING_SECRET: string;
+        SLACK_BOT_TOKEN: string;
+        SLACK_APP_TOKEN: string;
+        SLACK_LOGGING_LEVEL: any;
+    }>
+) => {
+    app.shortcut(
+        "delete_message",
+        async () => { },
+        async ({ context, payload }) => {
+            // check if the thread is authored by this bot
+            // @ts-expect-error - this is a bug in the library
+            if (payload.message.bot_id === process.env.SLACK_BOT_ID) {
+                // delete any messages that are in the thread
+                const thread = await context.client.conversations.replies({
+                    // @ts-expect-error - this is a bug in the library
+                    channel: payload.channel.id,
+                    // @ts-expect-error - this is a bug in the library
+                    ts: payload.message.ts,
+                });
+
+                // @ts-expect-error - this is a bug in the library
+                console.log(`🗑️  Deleting message ${payload.message.ts}${thread.messages!.length > 0 ? ` and ${thread.messages!.length - 1} other messages` : ""} in ${payload.channel.id}`);
+
+                for (const message of thread.messages!) {
+                    // check if the message is from the bot
+                    if (message.bot_id === process.env.SLACK_BOT_ID) {
+                        await context.client.chat.delete({
+                            // @ts-expect-error - this is a bug in the library
+                            channel: payload.channel.id,
+                            // @ts-expect-error - this is a bug in the library
+                            ts: message.ts,
+                        });
+                    }
+                }
+            }
+        }
+    );
+};
+
+export default deleteMessage;
--- features/index.ts
+++ features/index.ts
@@ -4,3 +4,4 @@ export { default as memberJoinHandlerP2 } from "./memberJoinHandlerP2";
 export { default as MemberJoinHandlerP3 } from "./memberJoinHandlerP3";
 export { default as MemberJoinHandlerP4 } from "./memberJoinHandlerP4";
 export { default as MemberJoinHandlerP5 } from "./memberJoinHandlerP5";
+export { default as deleteMessage } from "./deleteMessage";
