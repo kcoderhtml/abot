8ecce8b78ae2b205746e672869b618a397b227cd
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-06-28 17:26:12 -0400

    feat: Add appMention feature to handle responding to app mentions in a channel

--- features/appMention.ts
+++ features/appMention.ts
@@ -11,11 +11,35 @@ const appMention = async (
     // listen for new members joining the channel
     app.event('app_mention', async ({ context, payload }) => {
         console.log(`👏 ${payload.user} mentioned me`);
-        await context.client.chat.postMessage({
-            channel: payload.channel,
-            thread_ts: payload.event_ts,
-            text: `<@${payload.user}> if you want some help try asking my creator <@${process.env.CREATOR}>`,
-        });
+
+        let message: { message: string, ephemeral?: boolean, ping?: boolean } = { message: `<@${payload.user}> if you want some help try asking my creator <@${process.env.CREATOR}>`, ephemeral: true };
+
+        if (payload.text) {
+            const command = payload.text.split(' ').slice(1).join(' ')
+            switch (command) {
+                case 'ping':
+                    message = { message: `<@${payload.user}> pong!`, ephemeral: true };
+                    break;
+                case 'ping @here':
+                    message = { message: `<!here> pong!`, ping: true };
+                    break;
+            }
+        }
+
+        if (message.ephemeral) {
+            await context.client.chat.postEphemeral({
+                channel: payload.channel,
+                user: payload.user!,
+                text: message.message,
+            });
+        } else {
+            await context.client.chat.postMessage({
+                channel: payload.channel,
+                text: message.message,
+                thread_ts: message.ping ? payload.event_ts : undefined,
+                reply_broadcast: message.ping,
+            });
+        }
     });
 };
 
