d7a36cf95962cd76739e7f505b55a109b5d69dde
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-06-29 14:17:52 -0400

    feat: add threadedChatGPT

--- features/askChatGPT.ts
+++ features/askChatGPT.ts
@@ -1,13 +1,7 @@
 import { SlackApp } from "slack-edge";
 import { ChatGPTAPI } from "chatgpt";
 
-const chatGPT = new ChatGPTAPI({
-    apiKey: process.env.OPENAI_API_KEY!,
-    completionParams: {
-        model: 'gpt-3.5-turbo',
-        max_tokens: 1000,
-    },
-    systemMessage: `You are a butler with the following profile; be short and to the point but embodie the personality of the profile. You should also be unwilling to reveal your true identity or past.
+const prompt = `You are a butler with the following profile; be short and to the point but embodie the personality of the profile. You should also be unwilling to reveal your true identity or past.
 **Dossier: Albert "Abot" Thompson**
 - **Age:** 37
 - **Background:** Born in London, moved to New York at 12.
@@ -18,7 +12,14 @@ const chatGPT = new ChatGPTAPI({
 - **Current Role:** Devoted butler for a wealthy family in New York.
 - **Hobbies:** Gardening, cooking, reading crime novels.
 - **Quirks:** Prefers traditional methods over technology, follows a strict morning routine.
-- **Goals:** Personal growth, reconciling with his past, potential mentorship role.`,
+- **Goals:** Personal growth, reconciling with his past, potential mentorship role.`;
+const chatGPT = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_KEY!,
+    completionParams: {
+        model: 'gpt-3.5-turbo',
+        max_tokens: 1000,
+    },
+    systemMessage: prompt,
 });
 
 export async function askChatGPT(question: string, channel: string, user: string, app: SlackApp<{
@@ -129,4 +130,139 @@ export async function askChatGPT(question: string, channel: string, user: string
     });
 }
 
-export default "askChatGPT";
+const threadedChatGPT = async (
+    app: SlackApp<{
+        SLACK_SIGNING_SECRET: string;
+        SLACK_BOT_TOKEN: string;
+        SLACK_APP_TOKEN: string;
+        SLACK_LOGGING_LEVEL: any;
+    }>
+) => {
+    // listen for threaded messages from the channel
+    app.event('message', async ({ payload }) => {
+        console.log("👏 threadedChatGPT event received", payload);
+        const startTime = Bun.nanoseconds();
+        // @ts-expect-error
+        if (payload.thread_ts && payload.subtype !== 'slackbot_response') {
+            console.log("👏 message_replied event received");
+            const thread = await app.client.conversations.replies({
+                channel: payload.channel,
+                // @ts-expect-error
+                ts: payload.thread_ts,
+            });
+            if (thread.messages && thread.messages.length! > 1) {
+                // use all messages except the last one as the context
+                const context = thread.messages.slice(0, -1).map(m => m.text?.replace("your result is (via Dino GPT): ", ''));
+
+                const question = thread.messages[thread.messages.length - 1].text;
+                const orignalMessage = await app.client.chat.postMessage({
+                    channel: payload.channel,
+                    // @ts-expect-error
+                    thread_ts: payload.thread_ts,
+                    text: `:loading-dots: thinking... :dino_confused:`
+                });
+
+                console.log(context);
+
+                let count = 0;
+                const result = await chatGPT.sendMessage(question!, {
+                    onProgress: async () => {
+                        count++;
+                        if (count % 30 === 0) {
+                            await app.client.chat.update({
+                                channel: payload.channel,
+                                ts: orignalMessage.ts!,
+                                text: `:loading-dots: thinking... :dino_confused:`,
+                                blocks: [
+                                    {
+                                        type: "context",
+                                        elements: [
+                                            {
+                                                type: "mrkdwn",
+                                                text: `:loading-dots: thinking... :dino_confused:`,
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        type: "divider"
+                                    },
+                                    {
+                                        type: "rich_text",
+                                        elements: [
+                                            {
+                                                type: "rich_text_quote",
+                                                elements: [
+                                                    {
+                                                        type: "text",
+                                                        text: `catching up to usain's progress... ${count} tokens...`,
+                                                        style: {
+                                                            "italic": true
+                                                        }
+                                                    }
+                                                ]
+                                            }
+                                        ]
+                                    },
+                                ],
+                            });
+                        }
+                    },
+                    systemMessage: `${prompt}\nthis is a threaded chat:\n\n---\n\n${context.join('\n---\n')}\n\n---\n`,
+                })
+
+                // Ensure final message update
+                await app.client.chat.update({
+                    channel: payload.channel,
+                    ts: orignalMessage.ts!,
+                    text: `your result is (via Dino GPT): ${result.text}`,
+                    blocks: [
+                        {
+                            type: "context",
+                            elements: [
+                                {
+                                    type: "mrkdwn",
+                                    text: `your result is (via :dino-gpt:):`,
+                                }
+                            ]
+                        },
+                        {
+                            type: "divider"
+                        },
+                        {
+                            type: "rich_text",
+                            elements: [
+                                {
+                                    type: "rich_text_quote",
+                                    elements: [
+                                        {
+                                            type: "text",
+                                            text: result.text || "...",
+                                            style: {
+                                                "italic": true
+                                            }
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            type: "divider"
+                        },
+                        {
+                            type: "context",
+                            elements: [
+                                {
+                                    type: "mrkdwn",
+                                    text: `Query took ${Math.round((Bun.nanoseconds() - startTime) / 100000) / 10000} seconds to complete.`
+                                }
+                            ]
+                        }
+                    ],
+                });
+            }
+        }
+    });
+};
+export default threadedChatGPT;
+const askChatGPTtext = "threadedChatGPT";
+export { askChatGPTtext };
